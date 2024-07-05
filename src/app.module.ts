4a978b3898290b225a5f2499fc00bd429f3a1305
Ksanbal <dev.ksanbal@gmail.com>
2024-07-05 10:30:31 +0900

    :sparkles: Feat: Mock API 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QueueModule } from './presentation/queue/queue.module';
+import { ConcertModule } from './presentation/concert/concert.module';
+import { PointModule } from './presentation/point/point.module';
 
 @Module({
-  imports: [],
+  imports: [QueueModule, ConcertModule, PointModule],
   controllers: [AppController],
   providers: [AppService],
 })
